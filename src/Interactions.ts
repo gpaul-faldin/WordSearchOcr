864ea631461ddb063ab90843177aee621667c786
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2023-12-24 02:26:41 +0100

    Updated mouseDelay to make it more visual

--- src/Interactions.ts
+++ src/Interactions.ts
@@ -1,12 +1,12 @@
 import {moveMouse, mouseClick, setMouseDelay} from "robotjs";
 import {WordPosition} from "./checkWord";
 
-const DEFINED_X_OFFSET = 85;
-const DEFINED_Y_OFFSET = 310;
+const DEFINED_X_OFFSET = 86;
+const DEFINED_Y_OFFSET = 311;
 
 const moveAndClick = async (position: WordPosition) => {
 
-    await moveMouse(position.start.x + DEFINED_X_OFFSET, position.start.y + DEFINED_Y_OFFSET);
+    moveMouse(position.start.x + DEFINED_X_OFFSET, position.start.y + DEFINED_Y_OFFSET);
     mouseClick();
     moveMouse(position.end.x + DEFINED_X_OFFSET, position.end.y + DEFINED_Y_OFFSET);
     mouseClick();
@@ -14,11 +14,10 @@ const moveAndClick = async (position: WordPosition) => {
 
 export const CompleteGame = async (positions: WordPosition[]): Promise<Boolean> => {
 
-    setMouseDelay(5);
+    setMouseDelay(50);
 
     for (let i = 0; i < positions.length; i++) {
         await moveAndClick(positions[i]);
-        //return process.exit(0)
     }
     return true;
 }
